584c29e4db8ba8969e7221241cad99b44517f1d4
Emilia Sochiera <sochiera.emilia@gmail.com>
2023-11-04 23:42:01 +0100

    PageHeader components created & imported

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import "./App.css";
 import Post from "./Post";
 import Layout from "./Layout";
 import {Route, Routes} from "react-router-dom";
+import IndexPage from "./pages/IndexPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout/>}>
-        <Route index element={<Post/>}/>
+        <Route index element={<IndexPage/>}/>
         <Route path={'/login'} element={<div>Login page</div>}/>
       </Route>
     </Routes>
