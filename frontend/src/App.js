0dfd5d1046129734fc51c196b53f522c05a65f3e
Emilia Sochiera <sochiera.emilia@gmail.com>
2023-11-04 23:54:43 +0100

    login page created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,14 @@ import Post from "./Post";
 import Layout from "./Layout";
 import {Route, Routes} from "react-router-dom";
 import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout/>}>
         <Route index element={<IndexPage/>}/>
-        <Route path={'/login'} element={<div>Login page</div>}/>
+        <Route path="/login" element={<LoginPage/>}/>
       </Route>
     </Routes>
   );
