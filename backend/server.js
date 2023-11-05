dfe7da524ae5b066910c53a16914f6c6f50883e0
Emilia Sochiera <sochiera.emilia@gmail.com>
2023-11-05 20:47:13 +0100

    login request

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,36 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose')
-const User = require('./models/User');
-const bcrypt = require('bcryptjs')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/User");
+const bcrypt = require("bcryptjs");
 const app = express();
 
 const salt = bcrypt.genSaltSync(10);
 
-
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://emi-blog:j2XqeTaf8TZxUHs0@cluster0.9z86y2z.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(
+  "mongodb+srv://emi-blog:j2XqeTaf8TZxUHs0@cluster0.9z86y2z.mongodb.net/?retryWrites=true&w=majority"
+);
 
-app.post('/register', async(req, res) => {
-  const {username, password} = req.body;
-  try{
-  const userDoc = await User.create({
-    username,
-    password: bcrypt.hashSync(password,salt),
-  })
-  res.json(userDoc);
-} catch(e){
-  res.status(400).json(e)
-}
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
 });
 
 app.listen(3005);
-
-
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,12 +1,34 @@
-export default function LoginPage(){
-    return(
-        <div>
-            <form className="login" action="">
-            <h1>Login</h1>
-                <input type="text" placeholder="username"/>
-                <input type="password" placeholder="password"/>
-                <button>Login</button>
-            </form>
-        </div>
-    )
+import { useState } from "react";
+
+export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  async function login(ev){
+    ev.preventDefault();
+    await fetch('http://localhost:3005/login',{
+        method: "POST",
+        body: JSON.stringify({ username, password }),
+        headers: { "Content-Type": "application/json" },
+    })
+  }
+  return (
+    <div>
+      <form className="login" on Submit={login}>
+        <h1>Login</h1>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(ev) => setUsername(ev.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(ev) => setPassword(ev.target.value)}
+        />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
