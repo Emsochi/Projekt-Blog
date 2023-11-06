fa88f869c5f03b0fb0a8f2dc14b5d0254778f7e3
Emilia Sochiera <sochiera.emilia@gmail.com>
2023-11-06 23:18:42 +0100

    access to protected resources, such as your user profile, after logging in

--- frontend/src/PageHeader.js
+++ frontend/src/PageHeader.js
@@ -1,6 +1,16 @@
 import { Link } from "react-router-dom"
+import { useEffect } from "react";
 
 export default function PageHeader(){
+  useEffect(() =>{
+    fetch('http://localhost:3005/profile',{
+      credentials: 'include',
+    }).then(response => {
+       response.json().then(userInfo => {
+        
+      })
+    })
+  }, []);
     return(
         <header>
         <Link to="/" className="logo">My Blog</Link>
